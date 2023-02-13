c11d4b7e2eb371030ceeab8f30996e25234dfa60
mahekagg <105991553+mahekagg@users.noreply.github.com>
2023-02-13 17:08:34 -0500

    update 1

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+document.getElementById("point-button").button.addEventListener("click", function() {
+    let xval = document.getElementById("x").value;
+    let yval = document.getElementById("y").value;
+
+    let svg =  document.getElementById("frame")
+    point = document.createElement()
+
+});
+
+
+
+
+function clickPoint() {
+
+    let maxPt = document.getElementById("max");
+
+    maxPt.classList.toggle("border");
+
+}
