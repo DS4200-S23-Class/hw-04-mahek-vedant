06462acc907156b53ac75e37ca041922323a3b7b
vbhagat02 <vedant.bhagat0204@gmail.com>
2023-02-13 19:13:06 -0500

    fixed axis

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
 
 
-document.getElementById("point-button").button.addEventListener("click", function() {
-    let xval = document.getElementById("x").value;
-    let yval = document.getElementById("y").value;
+window.onload = function() {
+    document.getElementById("point-button").addEventListener("click", function() {
+        let svg = document.getElementById("frame");
+        let xval = document.getElementById("x").value;
+        let yval = document.getElementById("y").value;
+        let point = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
-    let svg =  document.getElementById("frame")
-    point = document.createElement()
+        point.setAttribute("cx", xval * 50);
+        point.setAttribute("cy", 500 - (yval * 50));
+        point.setAttribute("r", 10);
+        
+        // Adding new point to plot
+        svg.appendChild(point);
+        
+        // add on click event listener for newly created point
+        point.addEventListener("click", pointClicked(points[i]));
+        
+    });
+};
 
-});
-
-
-
-
-function clickPoint() {
-
-    let maxPt = document.getElementById("max");
-
-    maxPt.classList.toggle("border");
-
+function pointClicked(point) {
+    point.classList.toggle("border");
+    let output = document.getElementById('output');
+    output.style.display = "block";
+    output.innerHTML = "Selected Vis: " + points[i].value;
+        
 }
+
+
+
+// looping through all points and adding event listeners on click
+let points = document.getElementsByClassName("point");
+for (let i = 0; i < points.length; i++) {
+    console.log(points[i]);
+    points[i].addEventListener("click", () => pointClicked(points[i]));
+}
+
+// points.forEach((point) => {
+//     point.addEventListener("click", () => pointClicked(point));
+// });
+
+
+
+
+
